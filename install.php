1da6e1eb4587dffc466acb743d7d81b0b76d86c3
RobinDev <contact@piedweb.com>
2024-04-03 08:31:03 +0000

    prepare v1

--- install.php
+++ install.php
@@ -1,12 +1,13 @@
 <?php
 
+use Pushword\Installer\PostInstall;
+
 /**
  * Execute via Pushword\Installer\PostInstall::postUpdateCommand.
  */
-
-if (! \Pushword\Installer\PostInstall::isRoot()) {
+if (! PostInstall::isRoot()) {
     throw new Exception('installer mus be run from root');
 }
 
 echo '~~ Adding Routes'.chr(10);
-\Pushword\Installer\PostInstall::addOnTop('config/routes.yaml', "version:\n    resource: '@PushwordVersionBundle/VersionRoutes.yaml'\n");
+PostInstall::addOnTop('config/routes.yaml', "version:\n    resource: '@PushwordVersionBundle/VersionRoutes.yaml'\n");
