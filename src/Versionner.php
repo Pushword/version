035f4df1d3318de63334953cd8c415cafe3edf5a
RobinDev <contact@piedweb.com>
2026-03-25 09:28:52 +0000

    Add ignored_properties config to exclude custom properties from flat file export/import

--- src/Versionner.php
+++ src/Versionner.php
@@ -1,7 +1,5 @@
 <?php
 
-declare(strict_types=1);
-
 namespace Pushword\Version;
 
 use Doctrine\Bundle\DoctrineBundle\Attribute\AsDoctrineListener;
@@ -76,19 +74,7 @@ class Versionner
                 throw new Exception('Page not found `'.$pageId.'`');
             }
 
-            $oldSlug = $page->slug;
             $this->populate($page, $version);
-
-            // If slug changed, remove any redirect page that would conflict
-            if ($page->slug !== $oldSlug) {
-                $conflicting = $this->entityManager->getRepository(Page::class)->findOneBy([
-                    'slug' => $page->slug,
-                    'host' => $page->host,
-                ]);
-                if (null !== $conflicting && $conflicting->id !== $page->id && null !== $conflicting->getRedirection()) {
-                    $this->entityManager->remove($conflicting);
-                }
-            }
 
             $this->entityManager->flush();
         } finally {
